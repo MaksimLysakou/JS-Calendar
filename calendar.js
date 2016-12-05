191e3fb7bbf2d4deced5774f2e05bc1a1a36a39e
Максим <2008_96@mail.ru>
2016-12-05 16:43:45 +0300

    add daily repeats module

--- calendar.js
+++ calendar.js
@@ -16,119 +16,33 @@ function Event(name, dateTime, callback) {
     this.timeout = undefined;
 }
 
-function ExtendedEvent(name, dateTime, callback, repeats, preliminaryCallback, preliminaryDelay) {
-    Event.apply(this, arguments);
-
-    this.repeats = repeats;
-    this.preliminaryCallback = preliminaryCallback;
-    this.preliminaryDelay = preliminaryDelay;
-
-    this.preliminaryCallbackTimeout = undefined;
-}
-ExtendedEvent.prototype = Object.create(Event.prototype);
-
-function getNextExecuteDate (event) {
-    var nextExecuteDate = new Date(event.dateTime);
-
-    if(event.repeats === undefined || event.repeats.length == 0) {
-        return nextExecuteDate;
-    }else{
-        var currentDate = new Date();
-        if(event.repeats.indexOf(currentDate.getDay()) != -1){
-
-            nextExecuteDate.setFullYear(currentDate.getFullYear());
-            nextExecuteDate.setMonth(currentDate.getMonth());
-            nextExecuteDate.setDate(currentDate.getDate());
-
-            if(event.dateTime.getHours() > currentDate.getHours()){
-                return nextExecuteDate;
-            }
-
-            if(event.dateTime.getHours() == currentDate.getHours()){
-                if(event.dateTime.getMinutes() > currentDate.getMinutes()){
-                    return nextExecuteDate;
-                }
-
-                if(event.dateTime.getMinutes() == currentDate.getMinutes()){
-                    if(event.dateTime.getSeconds() > currentDate.getSeconds()){
-                        return nextExecuteDate;
-                    }
-
-                    if(event.dateTime.getSeconds() == currentDate.getSeconds()){
-                        if(event.dateTime.getMilliseconds() > currentDate.getMilliseconds())
-                            return nextExecuteDate;
-                    }
-                }
-            }
-        }
-
-        var nextExecuteDay = event.repeats[0];
-
-        for (var repeat in event.repeats){
-            if(repeat > currentDate.getDay()){
-                nextExecuteDay = repeat;
-                break;
-            }
-        }
-
-        nextExecuteDate.setFullYear(currentDate.getFullYear());
-        nextExecuteDate.setMonth(currentDate.getMonth());
-
-        if(nextExecuteDay > currentDate.getDay()){
-            nextExecuteDate.setDate(currentDate.getDate() + nextExecuteDay - currentDate.getDay());
-            return nextExecuteDate;
-        } else {
-            nextExecuteDate.setDate(currentDate.getDate() + nextExecuteDay - currentDate.getDay() + 7)
-            return nextExecuteDate;
-        }
-    }
-}
-
-
-//TODO: common event
+// function ExtendedEvent(name, dateTime, callback, repeats, preliminaryCallback, preliminaryDelay) {
+//     Event.apply(this, arguments);
+//
+//     this.repeats = repeats;
+//     this.preliminaryCallback = preliminaryCallback;
+//     this.preliminaryDelay = preliminaryDelay;
+//
+//     this.preliminaryCallbackTimeout = undefined;
+// }
+// ExtendedEvent.prototype = Object.create(Event.prototype);
 
 var Calendar = (function() {
 
     var events = [];
     var lastId = 0;
 
-    var startEventScheduler = function(){
-        events.forEach(function(event) {
-            if(event instanceof ExtendedEvent){
-                var nextExecute = getNextExecuteDate(event) - new Date();
-                if( ( nextExecute <= INT32_MAX ) &&
-                    ( nextExecute > 0 ) &&
-                    ( event.timeout === undefined ) ){
-                    event.timeout = setTimeout(event.callback, nextExecute);
-                    event.preliminaryCallbackTimeout =
-                        setTimeout(event.preliminaryCallback, nextExecute - event.preliminaryDelay);
-                }
-            }else{
-                if(event instanceof Event){
-                    if( ( event.dateTime - new Date() <= INT32_MAX ) &&
-                        ( event.dateTime - new Date() > 0 ) &&
-                        ( event.timeout === undefined ) ){
-                        event.timeout = setTimeout(event.callback, event.dateTime - new Date());
-                    }
-                }
+    function startEventScheduler() {
+        if(event instanceof Event){
+            if( ( event.dateTime - new Date() <= INT32_MAX ) &&
+                ( event.dateTime - new Date() > 0 ) &&
+                ( event.timeout === undefined ) ){
+                event.timeout = setTimeout(event.callback, event.dateTime - new Date());
             }
-
-
-        });
-    };
-    setInterval(startEventScheduler, 1000);
+        }
+    }
 
     function addEvent(event) {
-        if(event instanceof ExtendedEvent){
-            var nextExecute = getNextExecuteDate(event) - new Date();
-            if( (nextExecute >= 0) && (nextExecute <= INT32_MAX) ){
-                event.timeout = setTimeout(event.callback, nextExecute);
-                event.preliminaryCallbackTimeout = setTimeout(event.preliminaryCallback, nextExecute - event.preliminaryDelay);
-            }
-            event.id = ++lastId;
-            events.push(event);
-        }
-        else if(event instanceof Event){
             var timeout;
             if( (event.dateTime >= new Date()) && (event.dateTime - new Date() <= INT32_MAX) ){
                 timeout = setTimeout(event.callback, event.dateTime - new Date());
@@ -136,16 +50,20 @@ var Calendar = (function() {
             event.id = ++lastId;
             event.timeout = timeout;
             events.push(event);
-        }
+
+            if(this.isSchedulerStarted == false){
+                this.isSchedulerStarted = true;
+                setInterval(startEventScheduler, 1000*60);
+            }
     }
     function deleteEventById(eventId) {
         for(var i =0; i<events.length; i++) {
             if(events[i].id === eventId) {
                 clearTimeout(events[i].timeout);
-                //events[i].timeout = undefined; //In any case we remove this event
-                if(events[i] instanceof ExtendedEvent){
-                    clearTimeout(events[i].preliminaryCallbackTimeout);
-                }
+
+                // if(events[i] instanceof ExtendedEvent){
+                //     clearTimeout(events[i].preliminaryCallbackTimeout);
+                // }
                 events.splice(i, 1);
                 break;
             }
@@ -162,23 +80,20 @@ var Calendar = (function() {
 
                 if ((event.dateTime >= new Date()) && (event.dateTime - new Date() <= INT32_MAX)) {
                     event.timeout = setTimeout(event.callback, event.dateTime - new Date());
-                    // If we didn't set any timeout, set it as undefined
                 }
 
-                if (event instanceof ExtendedEvent) {
-                    clearTimeout(event.preliminaryCallbackTimeout);
-                    event.preliminaryCallbackTimeout = undefined;
-
-                    var nextExecute = getNextExecuteDate(event) - new Date();
-                    if ((nextExecute >= 0) && (nextExecute <= INT32_MAX)) {
-                        event.timeout = setTimeout(event.callback, nextExecute);
-                        event.preliminaryCallbackTimeout = setTimeout(event.preliminaryCallback, nextExecute - event.preliminaryDelay);
-                    }
-                }
+                // if (event instanceof ExtendedEvent) {
+                //     clearTimeout(event.preliminaryCallbackTimeout);
+                //     event.preliminaryCallbackTimeout = undefined;
+                //
+                //     var nextExecute = getNextExecuteDate(event) - new Date();
+                //     if ((nextExecute >= 0) && (nextExecute <= INT32_MAX)) {
+                //         event.timeout = setTimeout(event.callback, nextExecute);
+                //         event.preliminaryCallbackTimeout = setTimeout(event.preliminaryCallback, nextExecute - event.preliminaryDelay);
+                //     }
+                // }
             }
         });
-
-
     }
     function getAllEvents() {
         return events.slice();
@@ -225,49 +140,10 @@ var Calendar = (function() {
         getEventsByDay: getEventsByDay,
         getEventsByWeek: getEventsByWeek,
         getEventsByMonth: getEventsByMonth,
-        getEventsBetweenDates: getEventsBetweenDates
+        getEventsBetweenDates: getEventsBetweenDates,
+        startEventScheduler:  startEventScheduler
     };
 }());
 
+Calendar.isSchedulerStarted = false;
 
-function logDate() {
-    var currentDate = new Date();
-    console.log("["
-        + currentDate.getHours()
-        + ":" + currentDate.getMinutes()
-        + ":" + currentDate.getSeconds()
-        + "] " + "Callback happens"
-    );
-
-}
-//
-var date1 = new Date();
-date1.setSeconds(date1.getSeconds() + 5);
-//
-// var date2 = new Date();
-// date2.setSeconds(date2.getSeconds() + 15);
-//
-// var date3 = new Date();
-// date3.setSeconds(date3.getSeconds() + 60);
-//
-var extEvent = new ExtendedEvent("name2", date1, logDate, [1, 2], logDate, 2000);
-//
-// Calendar.addEvent(new Event("name1", new Date(1000, 10, 20), logDate));
- Calendar.addEvent(extEvent);
-// Calendar.addEvent(new Event("name3", date2, logDate));
-// Calendar.addEvent(new Event("name4", date3, logDate));
-// Calendar.addEvent(new Event("name5", new Date(2016, 12, 27), logDate));
-// карирование
-// string templates
-
-// findindex + polyfil
-
-var exDate = getNextExecuteDate(extEvent);
-console.log(
-    exDate.getDate() + "." +
-    exDate.getMonth() + "." +
-    exDate.getFullYear() + " " +
-    exDate.getHours() + ":" +
-    exDate.getMinutes() + ":" +
-    exDate.getSeconds()
-);
---
+++ calendarDailyRepeatsModule.js
@@ -0,0 +1,121 @@
+function getNextExecuteDate (event) {
+    var nextExecuteDate = new Date(event.dateTime);
+
+    if(event.repeats === undefined || event.repeats.length == 0) {
+        return nextExecuteDate;
+    }else{
+        var currentDate = new Date();
+        if(event.repeats.indexOf(currentDate.getDay()) != -1){
+
+            nextExecuteDate.setFullYear(currentDate.getFullYear());
+            nextExecuteDate.setMonth(currentDate.getMonth());
+            nextExecuteDate.setDate(currentDate.getDate());
+
+            if(event.dateTime.getHours() > currentDate.getHours()){
+                return nextExecuteDate;
+            }
+
+            if(event.dateTime.getHours() == currentDate.getHours()){
+                if(event.dateTime.getMinutes() > currentDate.getMinutes()){
+                    return nextExecuteDate;
+                }
+
+                if(event.dateTime.getMinutes() == currentDate.getMinutes()){
+                    if(event.dateTime.getSeconds() > currentDate.getSeconds()){
+                        return nextExecuteDate;
+                    }
+
+                    if(event.dateTime.getSeconds() == currentDate.getSeconds()){
+                        if(event.dateTime.getMilliseconds() > currentDate.getMilliseconds())
+                            return nextExecuteDate;
+                    }
+                }
+            }
+        }
+
+        var nextExecuteDay = event.repeats[0];
+
+        for (var repeat in event.repeats){
+            if(repeat > currentDate.getDay()){
+                nextExecuteDay = repeat;
+                break;
+            }
+        }
+
+        nextExecuteDate.setFullYear(currentDate.getFullYear());
+        nextExecuteDate.setMonth(currentDate.getMonth());
+
+        if(nextExecuteDay > currentDate.getDay()){
+            nextExecuteDate.setDate(currentDate.getDate() + nextExecuteDay - currentDate.getDay());
+            return nextExecuteDate;
+        } else {
+            nextExecuteDate.setDate(currentDate.getDate() + nextExecuteDay - currentDate.getDay() + 7)
+            return nextExecuteDate;
+        }
+    }
+}
+
+function Event(name, dateTime, callback) {
+    this.name = name;
+    this.dateTime = dateTime;
+    this.callback = callback;
+
+    this.id = undefined;
+    this.timeout = undefined;
+    this.repeats = undefined;
+}
+
+Calendar.startEventScheduler = function() {
+    var events = Calendar.getAllEvents();
+    
+    events.forEach(function (event) {
+        var nextExecute = getNextExecuteDate(event) - new Date();
+        if (( nextExecute <= INT32_MAX ) &&
+            ( nextExecute > 0 ) &&
+            ( event.timeout === undefined )) {
+            event.timeout = setTimeout(event.callback, nextExecute);
+        }
+    });
+};
+
+Calendar.addEvent = function (event) {
+    var events = Calendar.getAllEvents();
+
+    var nextExecute = getNextExecuteDate(event) - new Date();
+    if( (nextExecute >= 0) && (nextExecute <= INT32_MAX) ){
+        event.timeout = setTimeout(event.callback, nextExecute);
+    }
+
+    event.id = events.reduce(function(maxId, currentEvent) {
+        return (maxId < currentEvent.id) ? currentEvent.id : maxId;
+    }, 0) + 1;
+    events.push(event);
+
+    if(Calendar.isSchedulerStarted == false){
+        Calendar.isSchedulerStarted = true;
+        setInterval(Calendar.startEventScheduler, 6000);
+    }
+};
+
+Calendar.editEventById = function (eventId, name, date) {
+    var events = Calendar.getAllEvents();
+
+    events.forEach(function(event) {
+        if(event.id === eventId) {
+            if(name !== undefined)
+                event.name = name;
+
+            if(date !== undefined){
+                clearTimeout(event.timeout);
+                event.timeout = undefined;
+
+                event.dateTime = date;
+
+                var nextExecute = getNextExecuteDate(event) - new Date();
+                if ((nextExecute >= 0) && (nextExecute <= INT32_MAX)) {
+                    event.timeout = setTimeout(event.callback, nextExecute);
+                }
+            }
+        }
+    });
+};
